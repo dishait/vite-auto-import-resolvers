bfbc8efcb185f795e7789b6ba0109a91310bdefa
markthree <1801982702@qq.com>
2022-05-24 17:58:31 +0800

    fix: package some CJS only packages

--- tsup.config.ts
+++ tsup.config.ts
@@ -7,5 +7,6 @@ export default defineConfig({
 	splitting: true,
 	outDir: 'dist',
 	format: ['cjs', 'esm'],
-	entry: ['src/index.ts']
+	entry: ['src/index.ts'],
+	noExternal: ['case', 'fast-glob']
 })
