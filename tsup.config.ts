71b9fa21ba3f8cbc95bf6061c7fc32da85239392
markthree <1801982702@qq.com>
2022-11-05 14:56:22 +0800

    fix: fix dynamic require for esm

--- tsup.config.ts
+++ tsup.config.ts
@@ -8,5 +8,12 @@ export default defineConfig({
 	outDir: 'dist',
 	format: ['cjs', 'esm'],
 	entry: ['src/index.ts'],
-	noExternal: ['case', 'fast-glob']
+	noExternal: ['case', 'fast-glob'],
+	esbuildOptions: (options, { format }) => {
+		if (format === 'esm') {
+			options.banner = {
+				js: `import { createRequire } from 'module'; const require = createRequire(String(import.meta.url));`
+			}
+		}
+	}
 })
