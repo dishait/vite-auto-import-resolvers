9ad8c8da72035be6ed055c1684fcb9083daff635
markthree <1801982702@qq.com>
2023-01-08 19:15:43 +0800

    perf: add fileComputed to AutoGenerateImports

--- src/auto.ts
+++ src/auto.ts
@@ -1,5 +1,9 @@
 import { isPackageExists } from 'local-pkg'
 import { PresetName } from 'unplugin-auto-import/types'
+import {
+	normalizePath,
+	createFsComputedSync
+} from 'file-computed'
 
 interface Options {
 	/**
@@ -15,37 +19,45 @@ interface Options {
 export const AutoGenerateImports = (
 	options?: Partial<Options>
 ) => {
-	const {
-		include = [
-			'vue',
-			'pinia',
-			'vuex',
-			'vitest',
-			'vue-i18n',
-			'vue-router',
-			'@vueuse/core',
-			'@vueuse/head',
-			'@nuxtjs/composition-api',
-			'preact',
-			'quasar',
-			'react',
-			'react-router',
-			'react-router-dom',
-			'svelte',
-			'svelte/animate',
-			'svelte/easing',
-			'svelte/motion',
-			'svelte/store',
-			'svelte/transition',
-			'vitepress',
-			'vee-validate'
-		],
-		exclude = []
-	} = options || {}
-	return include.filter(preset => {
-		if (exclude.includes(preset)) {
-			return false
-		}
-		return isPackageExists(preset)
+	const packageJsonPath = normalizePath(
+		process.cwd(),
+		'package.json'
+	)
+	const fsComputed = createFsComputedSync()
+
+	return fsComputed(packageJsonPath, function () {
+		const {
+			include = [
+				'vue',
+				'pinia',
+				'vuex',
+				'vitest',
+				'vue-i18n',
+				'vue-router',
+				'@vueuse/core',
+				'@vueuse/head',
+				'@nuxtjs/composition-api',
+				'preact',
+				'quasar',
+				'react',
+				'react-router',
+				'react-router-dom',
+				'svelte',
+				'svelte/animate',
+				'svelte/easing',
+				'svelte/motion',
+				'svelte/store',
+				'svelte/transition',
+				'vitepress',
+				'vee-validate'
+			],
+			exclude = []
+		} = options || {}
+		return include.filter(preset => {
+			if (exclude.includes(preset)) {
+				return false
+			}
+			return isPackageExists(preset)
+		})
 	})
 }
