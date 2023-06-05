f81d1bddca83144757b3160556819fb793e5f928
markthree <1801982702@qq.com>
2023-06-05 13:43:29 +0800

    fix: remove cache auto, close #9

--- src/auto.ts
+++ src/auto.ts
@@ -1,63 +1,67 @@
-import { isPackageExists } from 'local-pkg'
-import { PresetName } from 'unplugin-auto-import/types'
-import {
-	normalizePath,
-	createFsComputedSync
-} from 'file-computed'
+import { isPackageExists } from "local-pkg";
+import type { PresetName } from "unplugin-auto-import/types";
 
 interface Options {
-	/**
-	 * @default ['vue', 'pinia', 'vuex','vitest','vue-i18n','vue-router','@vueuse/core','@vueuse/head','@nuxtjs/composition-api','preact','quasar','react','react-router','react-router-dom','svelte','svelte/animate','svelte/easing','svelte/motion','svelte/store','svelte/transition','vitepress','vee-validate']
-	 */
-	include: Array<PresetName>
-	/**
-	 * @default []
-	 */
-	exclude: Array<PresetName>
+  /**
+   * @default ["ahooks","@vueuse/core","@vueuse/math","@vueuse/head","mobx","mobx-react-lite","preact","quasar","react","react-router","react-router-dom","react-i18next","svelte","svelte/animate","svelte/easing","svelte/motion","svelte/store","svelte/transition","vee-validate","vitepress","vue-router","vue-router/composables","vuex","uni-app","solid-js","@solidjs/router","solid-app-router","jotai","jotai/utils","recoil","@vue/composition-api","pinia","vue-demi","vue-i18n","vue-router-composables","vue","vue/macros","vitest"]
+   */
+  include: Array<PresetName>;
+  /**
+   * @default []
+   */
+  exclude: Array<PresetName>;
 }
 
 export const AutoGenerateImports = (
-	options?: Partial<Options>
+  options?: Partial<Options>,
 ) => {
-	const packageJsonPath = normalizePath(
-		process.cwd(),
-		'package.json'
-	)
-	const fsComputed = createFsComputedSync()
-
-	return fsComputed(packageJsonPath, function () {
-		const {
-			include = [
-				'vue',
-				'pinia',
-				'vuex',
-				'vitest',
-				'vue-i18n',
-				'vue-router',
-				'@vueuse/core',
-				'@vueuse/head',
-				'@nuxtjs/composition-api',
-				'preact',
-				'quasar',
-				'react',
-				'react-router',
-				'react-router-dom',
-				'svelte',
-				'svelte/animate',
-				'svelte/easing',
-				'svelte/motion',
-				'svelte/store',
-				'svelte/transition',
-				'vitepress',
-				'vee-validate'
-			],
-			exclude = []
-		} = options || {}
-		return include.filter(preset => {
-			if (exclude.includes(preset)) {
-				return false
-			}
-			return isPackageExists(preset)
-		})
-	})
-}
+  const {
+    include = [
+      "ahooks",
+      "@vueuse/core",
+      "@vueuse/math",
+      "@vueuse/head",
+      "mobx",
+      "mobx-react-lite",
+      "preact",
+      "quasar",
+      "react",
+      "react-router",
+      "react-router-dom",
+      "react-i18next",
+      "svelte",
+      "svelte/animate",
+      "svelte/easing",
+      "svelte/motion",
+      "svelte/store",
+      "svelte/transition",
+      "vee-validate",
+      "vitepress",
+      "vue-router",
+      "vue-router/composables",
+      "vuex",
+      "uni-app",
+      "solid-js",
+      "@solidjs/router",
+      "solid-app-router",
+      "jotai",
+      "jotai/utils",
+      "recoil",
+      "@vue/composition-api",
+      "pinia",
+      "vue-demi",
+      "vue-i18n",
+      "vue-router-composables",
+      "vue",
+      "vue/macros",
+      "vitest",
+    ],
+    exclude = [],
+  } = options || {};
+  return (include as string[]).filter((preset: any) => {
+    if (exclude.includes(preset)) {
+      return false;
+    }
+    return isPackageExists(preset);
+  });
+};
